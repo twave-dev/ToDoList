b5c8f2d95c666940b4e163f07d53bfa1042c4f27
lotbaselog <lotbaselog@gmail.com>
2017-05-29 17:46:11 +0900

    URI Matcher 생성

--- app/src/main/java/com/twave/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/twave/todolist/data/TaskContentProvider.java
@@ -3,6 +3,7 @@ package com.twave.todolist.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -16,6 +17,22 @@ import android.support.annotation.Nullable;
 public class TaskContentProvider extends ContentProvider{
 
     TaskDbHelper mTaskDbHelper;
+
+    // TODO (3-1) Directory of tasks 와 a single item 을 구분하는 정수 상수 정의
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
+
+    // TODO (3-2) Uri Matcher 생성
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASK_WITH_ID);
+
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
